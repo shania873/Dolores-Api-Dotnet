52ea68cc66aa0d3171fae7c88d351637c56ee3ad
Caroline van Aerschot <vanaeca@hotmail.com>
2025-05-20 14:36:19 +0200

    add my trucs

---
+++ API_Dolores/Data/AppDbContextFactory.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace API_Dolores.Data
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+
+    public class AppDbContext : DbContext 
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        {
+        }
+    }
+}
---
+++ API_Dolores/Models/Task.cs
@@ -0,0 +1,6 @@
+﻿namespace API_Dolores.Models
+{
+    public class Task
+    {
+    }
+}
